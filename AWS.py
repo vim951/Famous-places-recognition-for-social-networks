cabb3a2d7dc2ce873911edcf9822e4d684891de5
Victor MASIAK <victor.masiak@telecom-paris.fr>
2020-12-04 17:18:58 +0100

    Multiprocessing

--- AWS.py
+++ AWS.py
@@ -8,18 +8,21 @@ import sys
 import os
 import shutil
 
+from multiprocessing import Process
+
 import database
 
 ## Constants
 
 VERBOSE,DEBUG=False,False
 
-#Those five values can be changed
+#Those values can be changed
 n=50
 size=100
-global_file_per_new_tar=10**4
+global_file_per_new_tar=10**12
 untar_dir='tmp1'
 npy_dir='tmp2'
+start_at=0
 
 #Those two values should only be changed in case of errors
 global_counter=0
@@ -43,9 +46,9 @@ def uploadNPY(filename):
     print('\033[96m------------------ UPLOAD ' + filename + ' -------------------\033[0m')
 
     s3_client = boto3.client('s3',
-    	aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
-    	aws_secret_access_key=os.environ.get('AWS_SECRET_KEY')
-    	)
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_KEY')
+        )
     bucket_size=sum([object.size for object in s3.Bucket('cassettefbisurveillancevan').objects.all()])
     print(bucket_size)
     if(bucket_size>3.5e9):
@@ -80,24 +83,17 @@ def dir_to_tar(path, tar_name):
                 tar_handle.add(os.path.join(root, file))
 
 def compress_send_wipe():
-    if VERBOSE:
-        print("compress_send_wipe() was called.")
     global global_counter
     global_counter=0
     database.preprocess_database()
     path=get_tar_name()
     dir_to_tar(npy_dir, path)
     send_database(path)
-    if DEBUG:
-        sys.exit(2)
-    else:
-        os.remove(path)
-        shutil.rmtree(npy_dir)
-        shutil.rmtree(untar_dir)
+    os.remove(path)
+    shutil.rmtree(npy_dir)
+    shutil.rmtree(untar_dir)
 
 def extract_image(tar, tarinfo):
-    if VERBOSE:
-        print("Extracting " + tarinfo.name)
     global global_counter
     tar.extract(tarinfo, untar_dir)
     global_counter += 1
@@ -105,8 +101,7 @@ def extract_image(tar, tarinfo):
         compress_send_wipe()
 
 def read_tar(path, must_delete):
-    if VERBOSE:
-        print("Reading " + str(path))
+    print("Process started for " + path)
     tar = tarfile.open(path)
     for tarinfo in tar:
         #print(tarinfo.name, "is", tarinfo.size, "bytes in size and is ", end="")
@@ -115,9 +110,7 @@ def read_tar(path, must_delete):
             if i in ids:
                 extract_image(tar, tarinfo)
     tar.close()
-    if DEBUG:
-        sys.exit(1)
-    elif must_delete:
+    if must_delete:
         os.remove(path)
 
 ## Init
@@ -125,8 +118,8 @@ def read_tar(path, must_delete):
 def init():
     global C,L,ids,s3
     s3 = boto3.resource('s3',
-    	aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
-    	aws_secret_access_key=os.environ.get('AWS_SECRET_KEY')
+        aws_access_key_id=os.environ.get('AWS_ACCESS_KEY'),
+        aws_secret_access_key=os.environ.get('AWS_SECRET_KEY')
     )
     print("Initialising")
     C,_=database.load_db_csv(n)
@@ -134,21 +127,27 @@ def init():
 
 def process_s3():
     init()
+    P=[]
     for i in range(500):
         k = str(i)
         k = "0"*(3-len(k))+k
-        print('Treating images_' + k + '.tar')
+        print('Donwloading images_' + k + '.tar and creating process')
         downloadTar('images_' + k + '.tar')
-        read_tar('tmp.tar', True)
+        P.append(Process(target=read_tar, args=['tmp.tar', True]))
+    for p in P:
+        p.start()
     compress_send_wipe()
 
 def process_local():
     init()
-    for i in range(500):
+    P=[]
+    for i in range(start_at, 500):
         k = str(i)
         k = "0"*(3-len(k))+k
-        print('Treating images_' + k + '.tar')
-        read_tar('data/images_' + k + '.tar', False)
+        print('Creating process for images_' + k + '.tar')
+        P.append(Process(target=read_tar, args=['data/images_' + k + '.tar', False]))
+    for p in P:
+        p.start()
     compress_send_wipe()
 
 if __name__ == "__main__":
