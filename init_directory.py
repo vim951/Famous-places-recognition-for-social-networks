be999fb5e728a1a14b40c11b86c81ad23f388347
Victor MASIAK <victor.masiak@telecom-paris.fr>
2020-12-04 12:35:56 +0100

    Required files from args + fail proof

--- init_directory.py
+++ init_directory.py
@@ -1,11 +1,27 @@
 import wget
+import sys
 
 def download_csv():
-    print("Downloading train_clean.csv")
-    wget.download('https://s3.amazonaws.com/google-landmark/metadata/train_clean.csv', 'train_clean.csv')
-    print("Downloading train_label_to_category.csv")
-    wget.download('https://s3.amazonaws.com/google-landmark/metadata/train_label_to_category.csv', 'train_label_to_category.csv')
+    try:
+        print("Downloading train_clean.csv")
+        wget.download('https://s3.amazonaws.com/google-landmark/metadata/train_clean.csv', 'train_clean.csv')
+    except:
+        print("train_clean.csv download failed")
+    try:
+        print("Downloading train_label_to_category.csv")
+        wget.download('https://s3.amazonaws.com/google-landmark/metadata/train_label_to_category.csv', 'train_label_to_category.csv')
+    except:
+        print("train_label_to_category.csv download failed")
     print("Done downloading")
 
 def download_pdb():
     pass
+
+if __name__ == "__main__":
+    for i in range(1,len(sys.argv)):
+        if sys.argv[i]=='csv':
+            download_csv()
+        elif sys.argv[i]=='pdb':
+            download_pdb()
+        else:
+            print("Unknown parameter: " + str(sys.argv[i]))
