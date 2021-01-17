deee1777a38b7987555b7eaa82db2f7211136a4f
Victor MASIAK <victor.masiak@telecom-paris.fr>
2021-01-17 17:07:01 +0100

    result to json

--- lambda.py
+++ lambda.py
@@ -3,7 +3,9 @@ from PIL import Image
 import numpy as np
 import wget
 from tensorflow import keras
+
 classes=['Noraduz Cemetery', 'Museum of Folk Architecture and Ethnography in Pyrohiv', 'Salève', 'Nieuwe Waterweg', 'Khotyn Fortress', 'Pešter', 'Niagara Falls', 'Haleakalā National Park', 'Feroz Shah Kotla', 'Catedral San Sebastián', 'Cochabamba', 'Grand Canyon', 'Golden Gate Bridge', 'Madrid Río', 'Kecharis', 'Mathura Museum', 'Hayravank monastery', 'Qutb Minar and its monuments', 'Delhi', 'Sofiyivsky Park', 'St. Lawrence', 'Toronto', 'Akkerman fortress', 'Edinburgh Castle', 'Eiffel Tower', 'Genoese fortress (Sudak)', 'Skopje Fortress', 'Masada', 'Matka Canyon', 'Pakistan Monument Islamabad', 'Purana Qila', 'Itmad-Ud-Daulahs Tomb', 'Faisal Mosque', 'Saint Holy Mother church of Yeghvard', 'Haghpat', 'Ogrodzieniec Castle', 'Grote of Onze-Lieve-Vrouwekerk (Breda)', 'York River State Park', 'Douthat State Park', 'St. Hripsime church in Vagharshapat', 'Lake Como', 'Ljubljana Castle', 'Victoria Memorial', 'Kolkata', 'Episcopal Diocese of Southwest Florida', 'Perperikon', 'Akbars Tomb', 'Harichavank', 'Jurassic Coast', 'Isa Khan Niyazis tomb', 'Nyhavn', 'Çufut Qale', 'Saiful Muluk Lake', 'SBB Historic']
+
 def preprocess_image_png(image_path, size=256):
     old_im = Image.open(image_path)
     (l,h) = old_im.size
@@ -19,6 +21,7 @@ def preprocess_image_png(image_path, size=256):
 def init(model_path):
     global model, labels
     model = keras.models.load_model(model_path)
+    
 def url_to_result(url):
     file_path = 'image.' + url.split('.')[-1]
     wget.download(url, file_path)
@@ -31,8 +34,8 @@ if __name__ == '__main__':
     path="https://upload.wikimedia.org/wikipedia/commons/c/c3/Saint_Holy_Mother_church_of_Yeghvard_01.jpg"
     init('./test_model')
     result = url_to_result(path)
-    list_result=sorted(list(zip(classes, result[0])), key=lambda x: -x[1])
-    print(list_result)
+    y=sorted(list(zip(classes, result[0])), key=lambda x: -x[1])
+    print(dict((i+1, dict((['landmark', y[i][0]], ['proba', y[i][1]]))) for i in range(len(y))))
+    
 def trigger_lambda(event, context):
-
     return event['url'];
